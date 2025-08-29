ebb09417e6d39fbb3ac6978b450f8399db5c154f
kamstand <kamrul.hassan@stonybrook.edu>
2025-08-29 05:57:23 -0400

    feat(scanner): increment line counter reaching \n and properly parse comments

--- src/lox/scanner.d
+++ src/lox/scanner.d
@@ -83,6 +83,24 @@ class Scanner
         case '>':
             addToken((match('=')) ? TokenType.GREATER_EQUAL : TokenType.GREATER);
             break;
+        case '/':
+            if (match('/'))
+            {
+                while (peek() != '\n' && !isAtEnd())
+                    advance();
+            }
+            else
+            {
+                addToken(TokenType.SLASH);
+            }
+            break;
+        case ' ':
+        case '\r':
+        case '\t':
+            break;
+        case '\n':
+            line++;
+            break;
         default:
             break;
         }
